4e3af97743874ebd61d8b23523d17682a57778de
Andrii Apostol <onix.avacom@gmail.com>
2025-09-19 02:21:16 +0300

    handling special characters
    =, /, \, &

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,13 +3,17 @@
 set -e
 FILENAME=$1
 
-if [ -z $FILENAME ]; then
+if [ -z "$FILENAME" ]; then
   echo 'The path to the file is required'
   exit 1
 fi
 
-while IFS='=' read -r -a var; do
-  echo "Setting ${var[0]} to ${var[1]} "
-  echo ${var[1]} | wc -l
-  sed -i "s|__${var[0]}__|${var[1]}|g" $FILENAME
-done < <(printenv)
+printenv | while IFS= read -r line; do
+  key="${line%%=*}"          # everything before the first =
+  value="${line#*=}"         # everything after the first =
+
+  # escape characters that break sed replacement
+  safe_value=$(printf '%s' "$value" | sed -e 's/[\/&\\]/\\&/g')
+
+  sed -i "s|__${key}__|${safe_value}|g" "$FILENAME"
+done
